fa37de77231e5a9bece75b3acbc336d76400b53f
jferrater <joffry.ferrater@gmail.com>
2020-08-29 16:02:39 +0200

    Added mongodb app configuration

--- src/main/java/com/github/jferrater/messagingservice/config/AppConfig.java
+++ src/main/java/com/github/jferrater/messagingservice/config/AppConfig.java
@@ -1,11 +1,32 @@
 package com.github.jferrater.messagingservice.config;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
+@EnableMongoRepositories(basePackages = "com.github.jferrater.messagingservice.repository")
 public class AppConfig {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoDbConnectionUri;
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create(mongoDbConnectionUri);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), databaseName);
+    }
 
     @Bean
     public ModelMapper modelMapper() {
